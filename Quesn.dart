8b386a22e432b97361622c891376fed94b0d877a
Moulay Imad <111585460+Imad015@users.noreply.github.com>
2026-03-25 02:20:24 +0100

    Add hard level questions and update scoring logic in QuizApp

--- Quesn.dart
+++ Quesn.dart
@@ -9,3 +9,10 @@ class Med {
   final String answer;
   Med({required this.text, required this.answer});
 }
+
+class Hard {
+  final String text;
+  final String answer;
+  String? hint;
+  Hard({required this.text, required this.answer , required this.hint});
+}
--- QuizApp.dart
+++ QuizApp.dart
@@ -73,7 +73,66 @@ void main() {
         print("the answer is false");
       }
     }
+  } else {
+    print("Sorry but you score is Not enough");
   }
-  print("the now score is ${score}");
-  print("Thank You my friend");
+  print("your new score is ${score}");
+  if (score >= 60) {
+    List<Hard> level3 = [
+      Hard(
+        text:
+            "Which planet in our solar system has a day that is longer than its year?"
+            "",
+        answer: "venus",
+        hint:
+            "It is the hottest planet in our solar system and rotates very slowly.",
+      ),
+      Hard(
+        text:
+            "What is the only metal that is liquid at standard room temperature?",
+        answer: "mercury",
+        hint:
+            "It was formerly used in medical thermometers and is also known as quicksilver.",
+      ),
+      Hard(
+        text: "Which country has the most natural lakes in the world?",
+        answer: "canada",
+        hint:
+            "This North American country contains about 60% of the world's total lakes.",
+      ),
+      Hard(
+        text: "What does 'HTTP' stand for in web addresses",
+        answer: "hypertext transfer protocol.",
+        hint:
+            "It is the foundation of data communication for the World Wide Web.",
+      ),
+      Hard(
+        text:
+            "What art movement is Salvador Dalí most closely associated with?",
+        answer: "surrealism",
+        hint:
+            "is works often feature dream-like, bizarre, and illogical scenes, like melting clocks.",
+      ),
+    ];
+    for (var i = 0; i < level3.length; i++) {
+      stdout.write(level3[i].text);
+      String? Q2 = stdin.readLineSync();
+      if (Q2!.toLowerCase() == level3[i].answer) {
+        score += 20;
+        print("correct answer");
+      } else {
+        print("the answer is false");
+      }
+    }
+  } else {
+    print("Sorry but you score is Not enough");
+  }
+  print("your final score is ${score}");
+  if (score == 205) {
+    print("your are the super man Thank you ");
+  } else if (score == 0) {
+    print("your are bed man");
+  } else {
+    print("thank you for play this game");
+  }
 }
